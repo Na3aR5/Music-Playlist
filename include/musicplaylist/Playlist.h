54213f4904cbdbe9e281b0b935fa7a94ba6612e0
Nazar Shylo <nazarshilo5@gmail.com>
2026-02-16 22:15:29 +0200

    Added generators, PlayFor method to play playlist

--- include/musicplaylist/Playlist.h
+++ include/musicplaylist/Playlist.h
@@ -4,11 +4,26 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <memory>
 #include <filesystem>
 
 namespace mspl {
 	class Playlist;
 
+	class PlaylistElement {
+	public:
+		PlaylistElement(const std::filesystem::directory_entry& entry);
+
+	public:
+		const std::filesystem::path& GetPath() const noexcept;
+		double GetDurationSeconds() const noexcept;
+
+	private:
+		double                           m_durationSeconds;
+		std::filesystem::directory_entry m_directoryEntry;
+	};
+
+	// pre-shuffled order
 	class PlaylistShuffled {
 	public:
 		class Iterator {
@@ -16,15 +31,15 @@ namespace mspl {
 			Iterator(
 				int current,
 				Playlist* playlist,
-				std::vector<int>* order
+				std::vector<size_t>* order
 			);
 
 		public:
-			std::filesystem::directory_entry& operator*() noexcept;
-			const std::filesystem::directory_entry& operator*() const noexcept {}
+			PlaylistElement& operator*() noexcept;
+			const PlaylistElement& operator*() const noexcept;
 
-			std::filesystem::directory_entry* operator->() noexcept;
-			const std::filesystem::directory_entry* operator->() const noexcept;
+			PlaylistElement* operator->() noexcept;
+			const PlaylistElement* operator->() const noexcept;
 
 			Iterator& operator++() noexcept;
 			Iterator& operator--() noexcept;
@@ -32,27 +47,129 @@ namespace mspl {
 			bool operator==(const Iterator& other) const noexcept;
 			bool operator!=(const Iterator& other) const noexcept;
 
+		public:
+			Iterator& Next() noexcept;
+			bool HasNext() const noexcept;
+
 		private:
-			int               m_current  = 0;
-			Playlist*		  m_playlist = nullptr;
-			std::vector<int>* m_order    = nullptr;
+			size_t			     m_current = 0;
+			Playlist* m_playlist = nullptr;
+			std::vector<size_t>* m_order = nullptr;
 		};
 
 	public:
 		PlaylistShuffled(
-			std::vector<int>&& order,
-			Playlist& playlist) : m_playlist(&playlist), m_order(std::move(order)) {}
+			Playlist* playlist,
+			std::vector<size_t>&& order
+		) : m_playlist(playlist), m_order(std::move(order)) {
+		}
 
 	public:
 		Iterator begin() noexcept;
 		Iterator end() noexcept;
 
 	private:
-		Playlist*        m_playlist;
-		std::vector<int> m_order;
+		Playlist* m_playlist;
+		std::vector<size_t> m_order;
 	};
 
+	// instead of using pre-generated order, generates next file on GenerateNext()
+	class PlaylistOrderGenerator {
+	public:
+		enum class GenerationStrategy {
+			ORDERED,
+			ORDERED_CYCLE,
+			RANDOM,
+			RANDOM_CYCLE,
+			RANDOM_CYCLE_REGENERATE,
+		};
+
+		class IGenerationStrategy {
+		public:
+			virtual ~IGenerationStrategy() = default;
+
+			virtual size_t GenerateNext() noexcept = 0;
+			virtual bool HasNext() const noexcept = 0;
+		};
+
+		class GenerationStrategy_Ordered : public IGenerationStrategy {
+		public:
+			GenerationStrategy_Ordered(size_t listSize);
+
+			virtual size_t GenerateNext() noexcept override;
+			virtual bool HasNext() const noexcept override;
+
+		private:
+			size_t m_current = 0;
+			size_t m_listSize;
+		};
+
+		class GenerationStrategy_OrderedCycle : public IGenerationStrategy {
+		public:
+			GenerationStrategy_OrderedCycle(size_t listSize);
+
+			virtual size_t GenerateNext() noexcept override;
+			virtual bool HasNext() const noexcept override;
+
+		private:
+			size_t m_current = 0;
+			size_t m_listSize;
+		};
+
+		class GenerationStrategy_Random : public IGenerationStrategy {
+		public:
+			GenerationStrategy_Random(size_t listSize);
+
+			virtual size_t GenerateNext() noexcept override;
+			virtual bool HasNext() const noexcept override;
+
+		private:
+			std::vector<size_t> m_order;
+		};
+
+		class GenerationStrategy_RandomCycle : public IGenerationStrategy {
+		public:
+			GenerationStrategy_RandomCycle(size_t listSize);
+
+			virtual size_t GenerateNext() noexcept override;
+			virtual bool HasNext() const noexcept override;
+
+		private:
+			size_t              m_current = 0;
+			std::vector<size_t> m_generatedOrder;
+			std::vector<size_t> m_order;
+		};
+
+		class GenerationStrategy_RandomCycleRegenerate : public IGenerationStrategy {
+		public:
+			GenerationStrategy_RandomCycleRegenerate(size_t listSize);
+
+			virtual size_t GenerateNext() noexcept override;
+			virtual bool HasNext() const noexcept override;
+
+		private:
+			size_t              m_current = 0;
+			size_t              m_listSize;
+			std::vector<size_t> m_generatedOrder;
+			std::vector<size_t> m_order;
+		};
+
+	public:
+		PlaylistOrderGenerator(Playlist* playlist, GenerationStrategy strategy);
+
+		PlaylistElement& GenerateNext() noexcept;
+		bool HasNext() const noexcept;
+
+	private:
+		std::unique_ptr<IGenerationStrategy> m_generationStrategy;
+		Playlist* m_playlist;
+	};
+
+	// set of files
 	class Playlist {
+	public:
+		using PlayStrategy = PlaylistOrderGenerator::GenerationStrategy;
+
 	public:
 		Playlist(
 			const std::filesystem::path& directory,
@@ -60,15 +177,23 @@ namespace mspl {
 		);
 
 	public:
-		PlaylistShuffled GetShuffled() noexcept;
+		size_t GetSize() const noexcept;
+
+		PlaylistShuffled GetShuffled();
+		PlaylistOrderGenerator GetOrderGenerator(PlaylistOrderGenerator::GenerationStrategy strategy);
 
 	public:
-		std::vector<std::filesystem::directory_entry>::iterator begin() noexcept;
-		std::vector<std::filesystem::directory_entry>::iterator end() noexcept;
+		void PlayFor(double seconds, PlayStrategy strategy);
+
+	public:
+		std::vector<PlaylistElement>::iterator begin() noexcept;
+		std::vector<PlaylistElement>::iterator end() noexcept;
 
 	private:
-		std::vector<std::filesystem::directory_entry> m_files;
-		std::mt19937								  m_randomGenerator;
+		void _PlayElement(const PlaylistElement& elem) noexcept;
+
+	private:
+		std::vector<PlaylistElement> m_elements;
 	};
 }
 
--- src/Playlist.cpp
+++ src/Playlist.cpp
@@ -1,20 +1,45 @@
 #include <musicplaylist/Playlist.h>
+#include <thread>
 
 #include <set>
-#include <list>
 
-mspl::PlaylistShuffled::Iterator::Iterator(int current, Playlist* playlist, std::vector<int>* order) :
-m_current(current), m_playlist(playlist), m_order(order) {}
+namespace {
+	std::mt19937 g_RandomGenerator{ std::random_device()() };
+}
 
-std::filesystem::directory_entry& mspl::PlaylistShuffled::Iterator::operator*() noexcept {
+// ---------------------------------------------------- PlaylistElement ----------------------------------------------------
+
+mspl::PlaylistElement::PlaylistElement(const std::filesystem::directory_entry& entry) :
+	m_directoryEntry(entry), m_durationSeconds(1.0) {
+}
+
+const std::filesystem::path& mspl::PlaylistElement::GetPath() const noexcept {
+	return m_directoryEntry.path();
+}
+
+double mspl::PlaylistElement::GetDurationSeconds() const noexcept {
+	return m_durationSeconds;
+}
+
+// ----------------------------------------------- PlaylistShuffled::Iterator ----------------------------------------------
+
+mspl::PlaylistShuffled::Iterator::Iterator(int current, Playlist* playlist, std::vector<size_t>* order) :
+	m_current(current), m_playlist(playlist), m_order(order) {
+}
+
+mspl::PlaylistElement& mspl::PlaylistShuffled::Iterator::operator*() noexcept {
 	return *(m_playlist->begin() + (*m_order)[m_current]);
 }
 
-std::filesystem::directory_entry* mspl::PlaylistShuffled::Iterator::operator->() noexcept {
+const mspl::PlaylistElement& mspl::PlaylistShuffled::Iterator::operator*() const noexcept {
+	return *(m_playlist->begin() + (*m_order)[m_current]);
+}
+
+mspl::PlaylistElement* mspl::PlaylistShuffled::Iterator::operator->() noexcept {
 	return (m_playlist->begin() + (*m_order)[m_current]).operator->();
 }
 
-const std::filesystem::directory_entry* mspl::PlaylistShuffled::Iterator::operator->() const noexcept {
+const mspl::PlaylistElement* mspl::PlaylistShuffled::Iterator::operator->() const noexcept {
 	return (m_playlist->begin() + (*m_order)[m_current]).operator->();
 }
 
@@ -36,53 +61,264 @@ bool mspl::PlaylistShuffled::Iterator::operator!=(const Iterator& other) const n
 	return !(m_current == other.m_current && m_playlist == other.m_playlist && m_order == other.m_order);
 }
 
-const std::filesystem::directory_entry& mspl::PlaylistShuffled::Iterator::operator*() const noexcept {
-	return *(m_playlist->begin() + (*m_order)[m_current]);
+mspl::PlaylistShuffled::Iterator& mspl::PlaylistShuffled::Iterator::Next() noexcept {
+	++m_current;
+	return *this;
 }
+
+bool mspl::PlaylistShuffled::Iterator::HasNext() const noexcept {
+	return static_cast<size_t>(m_current + 1) < m_order->size();
+}
+
+// ---------------------------------------------------- PlaylistShuffled ---------------------------------------------------
 
 mspl::PlaylistShuffled::Iterator mspl::PlaylistShuffled::begin() noexcept {
 	return Iterator(0, m_playlist, &m_order);
 }
 
 mspl::PlaylistShuffled::Iterator mspl::PlaylistShuffled::end() noexcept {
-	return Iterator(m_order.size(), m_playlist, &m_order);
+	return Iterator(static_cast<int>(m_order.size()), m_playlist, &m_order);
 }
 
-mspl::Playlist::Playlist(const std::filesystem::path& directory, const std::vector<std::string>& fileExtensions) :
-m_randomGenerator(std::random_device()()) {
+// ------------------------------------------------ GenerationStrategy_Ordered ---------------------------------------------
+
+mspl::PlaylistOrderGenerator::GenerationStrategy_Ordered::GenerationStrategy_Ordered(size_t listSize) :
+	m_listSize(listSize) {
+}
+
+size_t mspl::PlaylistOrderGenerator::GenerationStrategy_Ordered::GenerateNext() noexcept {
+	return m_current++;
+}
+
+bool mspl::PlaylistOrderGenerator::GenerationStrategy_Ordered::HasNext() const noexcept {
+	return m_current < m_listSize;
+}
+
+// ---------------------------------------------- GenerationStrategy_OrderedCycle ------------------------------------------
+
+mspl::PlaylistOrderGenerator::GenerationStrategy_OrderedCycle::GenerationStrategy_OrderedCycle(size_t listSize) :
+	m_listSize(listSize) {
+}
+
+size_t mspl::PlaylistOrderGenerator::GenerationStrategy_OrderedCycle::GenerateNext() noexcept {
+	size_t result = m_current;
+	m_current = (m_current + 1) % m_listSize;
+	return result;
+}
+
+bool mspl::PlaylistOrderGenerator::GenerationStrategy_OrderedCycle::HasNext() const noexcept {
+	return true;
+}
+
+// ------------------------------------------------ GenerationStrategy_Random ----------------------------------------------
+
+mspl::PlaylistOrderGenerator::GenerationStrategy_Random::GenerationStrategy_Random(size_t listSize) {
+	m_order.reserve(listSize);
+	for (size_t i = 0; i < listSize; ++i) {
+		m_order.emplace_back(i);
+	}
+}
+
+size_t mspl::PlaylistOrderGenerator::GenerationStrategy_Random::GenerateNext() noexcept {
+	if (m_order.size() == 1) {
+		size_t result = m_order[0];
+		m_order.pop_back();
+		return result;
+	}
+
+	std::uniform_int_distribution<size_t> dist(0, m_order.size() - 1);
+	size_t index = dist(g_RandomGenerator);
+	size_t result = m_order[index];
+
+	std::swap(m_order.back(), m_order[index]);
+	m_order.pop_back();
+
+	return result;
+}
+
+bool mspl::PlaylistOrderGenerator::GenerationStrategy_Random::HasNext() const noexcept {
+	return !m_order.empty();
+}
+
+// ---------------------------------------------- GenerationStrategy_RandomCycle -------------------------------------------
+
+mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycle::GenerationStrategy_RandomCycle(size_t listSize) {
+	m_order.reserve(listSize);
+	for (size_t i = 0; i < listSize; ++i) {
+		m_order.emplace_back(i);
+	}
+}
+
+size_t mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycle::GenerateNext() noexcept {
+	if (m_order.size() == 1) {
+		size_t result = m_order[0];
+		m_generatedOrder.emplace_back(result);
+		m_order.pop_back();
+		return result;
+	}
+	if (!m_order.empty()) {
+		std::uniform_int_distribution<size_t> dist(0, m_order.size() - 1);
+		size_t index = dist(g_RandomGenerator);
+		size_t result = m_order[index];
+
+		m_generatedOrder.emplace_back(result);
+		std::swap(m_order.back(), m_order[index]);
+		m_order.pop_back();
+
+		return result;
+	}
+	size_t current = m_current;
+	m_current = (m_current + 1) % m_generatedOrder.size();
+	return m_generatedOrder[current];
+}
+
+bool mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycle::HasNext() const noexcept {
+	return true;
+}
+
+// ----------------------------------------- GenerationStrategy_RandomCycleRegenerate --------------------------------------
+
+mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycleRegenerate::GenerationStrategy_RandomCycleRegenerate(size_t listSize) :
+	m_listSize(listSize) {
+	m_order.reserve(m_listSize);
+	for (size_t i = 0; i < m_listSize; ++i) {
+		m_order.emplace_back(i);
+	}
+}
+
+size_t mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycleRegenerate::GenerateNext() noexcept {
+	if (m_order.size() == 1) {
+		size_t result = m_order[0];
+		m_generatedOrder.emplace_back(result);
+		m_order.pop_back();
+
+		// reset
+		for (size_t i = 0; i < m_listSize; ++i) {
+			m_order.emplace_back(i);
+		}
+		return result;
+	}
+	std::uniform_int_distribution<size_t> dist(0, m_order.size() - 1);
+	size_t index = dist(g_RandomGenerator);
+	size_t result = m_order[index];
+
+	m_generatedOrder.emplace_back(result);
+	std::swap(m_order.back(), m_order[index]);
+	m_order.pop_back();
+
+	return result;
+}
+
+bool mspl::PlaylistOrderGenerator::GenerationStrategy_RandomCycleRegenerate::HasNext() const noexcept {
+	return true;
+}
+
+// -------------------------------------------------- PlaylistOrderGenerator ------------------------------------------------
+
+mspl::PlaylistOrderGenerator::PlaylistOrderGenerator(Playlist* playlist, GenerationStrategy strategy) :
+	m_playlist(playlist) {
+	switch (strategy) {
+	case GenerationStrategy::ORDERED:
+		m_generationStrategy = std::make_unique<GenerationStrategy_Ordered>(m_playlist->GetSize());
+		break;
+
+	case GenerationStrategy::ORDERED_CYCLE:
+		m_generationStrategy = std::make_unique<GenerationStrategy_OrderedCycle>(m_playlist->GetSize());
+		break;
+
+	case GenerationStrategy::RANDOM:
+		m_generationStrategy = std::make_unique<GenerationStrategy_Random>(m_playlist->GetSize());
+		break;
+
+	case GenerationStrategy::RANDOM_CYCLE:
+		m_generationStrategy = std::make_unique<GenerationStrategy_RandomCycle>(m_playlist->GetSize());
+		break;
+
+	case GenerationStrategy::RANDOM_CYCLE_REGENERATE:
+		m_generationStrategy = std::make_unique<GenerationStrategy_RandomCycleRegenerate>(m_playlist->GetSize());
+		break;
+	}
+}
+
+mspl::PlaylistElement& mspl::PlaylistOrderGenerator::GenerateNext() noexcept {
+	return *(m_playlist->begin() + m_generationStrategy->GenerateNext());
+}
+
+bool mspl::PlaylistOrderGenerator::HasNext() const noexcept {
+	return m_generationStrategy->HasNext();
+}
+
+// --------------------------------------------------------- Playlist -------------------------------------------------------
+
+mspl::Playlist::Playlist(const std::filesystem::path& directory, const std::vector<std::string>& fileExtensions) {
 	std::set<std::string> extensionSet(fileExtensions.begin(), fileExtensions.end());
 
 	for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator(directory)) {
 		if (entry.is_regular_file() && extensionSet.find(entry.path().extension().string()) != extensionSet.cend()) {
-			m_files.emplace_back(entry);
+			m_elements.emplace_back(entry);
 		}
 	}
 }
 
-mspl::PlaylistShuffled mspl::Playlist::GetShuffled() noexcept {
-	std::vector<int> originOrder;
+size_t mspl::Playlist::GetSize() const noexcept {
+	return m_elements.size();
+}
 
-	for (int i = 0; i < m_files.size(); ++i) {
+mspl::PlaylistShuffled mspl::Playlist::GetShuffled() {
+	std::vector<size_t> originOrder;
+
+	originOrder.reserve(m_elements.size());
+	for (size_t i = 0; i < m_elements.size(); ++i) {
 		originOrder.emplace_back(i);
 	}
-	std::uniform_int_distribution<int> dist(0, originOrder.size());
+	std::uniform_int_distribution<size_t> dist;
 
-	std::vector<int> shuffledOrder;
+	std::vector<size_t> shuffledOrder;
 	while (!originOrder.empty()) {
-		int index = dist(m_randomGenerator);
-		shuffledOrder.emplace_back(index);
+		dist.param(std::uniform_int_distribution<size_t>::param_type(0, originOrder.size() - 1));
+
+		size_t index = dist(g_RandomGenerator);
+		shuffledOrder.emplace_back(originOrder[index]);
+
 		std::swap(originOrder.back(), originOrder[index]);
 		originOrder.pop_back();
-		dist.param(std::uniform_int_distribution<int>::param_type(0, originOrder.size()));
 	}
 
-	return PlaylistShuffled(std::move(shuffledOrder), *this);
+	return PlaylistShuffled(this, std::move(shuffledOrder));
 }
 
-std::vector<std::filesystem::directory_entry>::iterator mspl::Playlist::begin() noexcept {
-	return m_files.begin();
+mspl::PlaylistOrderGenerator mspl::Playlist::GetOrderGenerator(PlaylistOrderGenerator::GenerationStrategy strategy) {
+	return PlaylistOrderGenerator(this, strategy);
 }
 
-std::vector<std::filesystem::directory_entry>::iterator mspl::Playlist::end() noexcept {
-	return m_files.end();
+void mspl::Playlist::PlayFor(double seconds, PlayStrategy strategy) {
+	PlaylistOrderGenerator generator(this, strategy);
+
+	auto start = std::chrono::steady_clock::now();
+	while (generator.HasNext()) {
+		auto end = std::chrono::steady_clock::now();
+		double duration = std::chrono::duration<double>(end - start).count();
+
+		const PlaylistElement& elem = generator.GenerateNext();
+
+		if (duration + elem.GetDurationSeconds() >= seconds) {
+			break;
+		}
+		_PlayElement(elem);
+
+		std::this_thread::sleep_for(std::chrono::duration<double>(elem.GetDurationSeconds()));
+	}
+}
+
+std::vector<mspl::PlaylistElement>::iterator mspl::Playlist::begin() noexcept {
+	return m_elements.begin();
+}
+
+std::vector<mspl::PlaylistElement>::iterator mspl::Playlist::end() noexcept {
+	return m_elements.end();
+}
+
+#include <iostream>
+void mspl::Playlist::_PlayElement(const PlaylistElement& elem) noexcept {
+	std::cout << elem.GetPath() << '\n';
 }
